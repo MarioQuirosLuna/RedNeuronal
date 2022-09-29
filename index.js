e873b8116441be162ab526041d0213ba935446bf
Mario Quiros Luna <marioql.dev@gmail.com>
2022-09-29 15:59:51 -0600

    Neural Net using brain.js

---
+++ index.js
@@ -0,0 +1,29 @@
+let color = document.getElementById("color");
+let site = document.getElementById("site");
+
+let net = new brain.NeuralNetwork();
+net.train([
+    { input: { red: 0, green: 0, blue: 0 }, output: { color: 1 } },
+    { input: { red: 1, green: 1, blue: 1 }, output: { color: 0 } },
+    { input: { red: 0, green: 1, blue: 0 }, output: { color: 0 } },
+    { input: { red: 1, green: 0, blue: 0 }, output: { color: 0 } },
+])
+
+color.addEventListener("change", function () {
+    site.style.backgroundColor = color.value;
+
+    let rgb = color.value.substring(4, color.value.length - 1).split(",");
+    let data = {
+        red: rgb[0] / 255,
+        green: rgb[1] / 255,
+        blue: rgb[2] / 255
+    }
+
+    let result = net.run(data);
+
+    if (result.color > .5) {
+        site.style.color = "white"
+    } else {
+        site.style.color = "black"
+    }
+})
